4b47a2859cde79e4928e2e628a0cbaa88f29b949
ValleyKnight <ValleyKnight@protonmail.com>
2021-01-20 07:53:29 -0500

    better memory calculation

--- src/memory.cpp
+++ src/memory.cpp
@@ -6,13 +6,8 @@ std::string memory() {
     std::string total_line = parse("MemTotal", "/proc/meminfo");
     std::vector<std::string> total_line_vector = explode(total_line, ' ');
     int total_size = std::stoi(total_line_vector[1]);
-    if (total_size > 10000) {
-        int total = total_size / 10000;
-        std::string message_total = std::to_string(total);
-        std::string message = message_total.substr(0,2) + "GB";
-        return message;
-    } else if (total_size > 1000) {
-        int total = total_size / 1000;
+    if (total_size > 1024) {
+        int total = total_size / 1024;
         std::string message_total = std::to_string(total);
         std::string message = message_total + "MB";
         return message;

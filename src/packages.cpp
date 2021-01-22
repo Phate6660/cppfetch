471ee1924f2d9ab718fedf77fb067939efc7df41
ValleyKnight <ValleyKnight@protonmail.com>
2021-01-22 13:42:55 -0500

    refactor package stuff

--- src/packages.cpp
+++ src/packages.cpp
@@ -1,6 +1,21 @@
 #include <fstream>
 #include <iostream>
 
+std::string count(std::string cmd, std::string manager) {
+    const std::string& command = cmd + "| wc -l";
+    std::system((command + " > temp.txt").c_str());
+ 
+    std::ifstream ifs("temp.txt");
+    std::string ret{ std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>() };
+    ifs.close(); // must close the inout stream so the file can be cleaned up
+    if (std::remove("temp.txt") != 0) {
+        perror("Error deleting temporary file");
+    }
+    std::string message = ret + " (" + manager + ")";
+    message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
+    return message;
+}
+
 std::string packages() {
     // pkg -- emerge frontend written in Rust.
     if (std::system("which pkg > /dev/null 2>&1")) {
@@ -9,31 +24,11 @@ std::string packages() {
             // Nothing found.
             return "N/A (no support package managers found)";
         } else {
-            const std::string& command = "qlist -I | wc -l";
-            std::system((command + " > temp.txt").c_str());
- 
-            std::ifstream ifs("temp.txt");
-            std::string ret{ std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>() };
-            ifs.close(); // must close the inout stream so the file can be cleaned up
-            if (std::remove("temp.txt") != 0) {
-                perror("Error deleting temporary file");
-            }
-            std::string message = ret + " (Portage)";
-            message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
+            std::string message{ count("qlist -I", "Portage") };
             return message;
         }
     } else {
-        const std::string& command = "pkg -l | wc -l";
-        std::system((command + " > temp.txt").c_str());
- 
-        std::ifstream ifs("temp.txt");
-        std::string ret{ std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>() };
-        ifs.close(); // must close the inout stream so the file can be cleaned up
-        if (std::remove("temp.txt") != 0) {
-            perror("Error deleting temporary file");
-        }
-        std::string message = ret + " (Portage)";
-        message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
+        std::string message{ count("pkg -l", "Portage") };
         return message;
     }
 }
